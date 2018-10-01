6d28ab46af95f0adaabf58759561f994b089dba9
Cameron Kelley <cameron@kelley.vc>
2018-09-30 23:44:18 -0500

    documentation for deck.py

--- Deck.py
+++ Deck.py
@@ -11,6 +11,10 @@ class Deck:
         self.discard_pile = []
 
     def build_and_shuffle(self):
+        """
+        Build a deck from different card
+        combinations and shuffle it.
+        """
         cards = [] # lower index = top of deck
         for color in colors:
             for number in numbers:
@@ -27,18 +31,28 @@ class Deck:
         return cards
 
     def get_draw_pile_count(self):
+        """
+        Count the cards in the draw pile.
+        """
         return len(self.draw_pile)
 
     def get_top_card(self):
+        """
+        Return a pointer to the card on
+        top of the pile. Does not modify array.
+        """
         count = len(self.discard_pile)
         if count == 0:
             return None
         return self.discard_pile[count - 1]
 
-    def can_draw(self):
-        return len(self.draw_pile) > 0
-
     def draw(self):
+        """
+        Draw a card from the draw pile.
+        If no cards are left, replace the draw pile
+        with a shuffled version of the cards on
+        the bottom of the discard pile.
+        """
         if len(self.draw_pile) > 0:
             return self.draw_pile.pop(0)
         else:
@@ -48,16 +62,25 @@ class Deck:
 
             random.shuffle(replacable_cards)
             self.draw_pile = self.draw_pile + replacable_cards
-            
+
             del self.discard_pile[0:count_discard_pile-2]
             return self.draw_pile.pop(0)
 
 
     def discard(self, card):
+        """
+        Discard an instance of the card.
+        Places it on top of the discard pile.
+        Modifies the underlying discard_pile array.
+        Returns a boolean telling whether or not the card
+        could be discarded (based on the game rules)
+
+        @return False   the card could not be discarded
+        @return True    the card could be discarded
+        """
         # Allow discarding for the first card
         if len(self.discard_pile) == 0:
             self.discard_pile.append(card)
-
 
         # Check to make sure we're allowed to discard
         top_card = self.get_top_card()
@@ -65,5 +88,4 @@ class Deck:
             self.discard_pile.append(card)
             return True
         else:
-            # print("You can't discard this card!")
             return False
